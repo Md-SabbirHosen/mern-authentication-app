f902486d164d7ad2e44c9501c326ad498fd1f1a7
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-10-20 15:28:07 +0600

    setup server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
