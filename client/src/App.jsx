c7ad55dfff4911a4ff2b6c137c5cefaddcb2c555
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-10-22 02:54:21 +0600

    setup shadcn ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-cyan-400">Hello React</h1>
-    </>
-  );
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Registration from "./pages/Registration.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    children: [
+      {
+        index: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/registration",
+        element: <Registration />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ client/src/assets/index.js
@@ -0,0 +1,4 @@
+import Background from "@/assets/Background.svg";
+import Vector from "@/assets/Vector.svg";
+
+export { Background, Vector };
