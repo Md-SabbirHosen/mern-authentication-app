2fbb851f7aaefb00765789b2451ed64a219cff97
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-12-09 16:29:43 +0600

    add background image and shape

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -1,9 +1,39 @@
+import bgGradient from "@/assets/background-gradient.png";
+import FloatingShape from "@/components/FloatingShape";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden grid place-items-center bg-gradient-to-br from-[#4568DC] to-[#B06AB3] ">
-      <div className=" mt-2 z-20   ">
+    <div className="relative h-screen w-screen overflow-hidden  bg-[#1a2435]  ">
+      <div>
+        <img
+          src={bgGradient}
+          className="absolute w-4/5 top-0 left-0  opacity-20"
+          alt="bgGradients"
+        />
+      </div>
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+      <div className=" absolute top-5 right-1/4 ">
         <Outlet />
       </div>
     </div>
